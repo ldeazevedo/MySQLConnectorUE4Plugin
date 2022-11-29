b2b4ad2992391169a51c16a35a2eed100fa2e213
Leonardo Emmmanuel de Azevedo <ldeazevedo112@gmail.com>
2022-11-29 12:44:07 +0100

    Added support for Linux with MySQL database
    The original code uses mariadb. This change allows the use of mysql client library in Linux machines (for my dedicated server)

--- Source/MySQLConnectorUE4Plugin/MySQLConnectorUE4Plugin.Build.cs
+++ Source/MySQLConnectorUE4Plugin/MySQLConnectorUE4Plugin.Build.cs
@@ -5,9 +5,6 @@ public class MySQLConnectorUE4Plugin : ModuleRules
 {
     public MySQLConnectorUE4Plugin(TargetInfo Target)
     {
-        //File.WriteAllText("c:/temp/qqq.txt", this.GetType().Name);
-        //string ModulePath = Path.GetDirectoryName( RulesAssembly.GetModuleFilename( this.GetType().Name ) );
-
         UEBuildConfiguration.bForceEnableExceptions = true;
 
         RulesAssembly r;
@@ -18,14 +15,20 @@ public class MySQLConnectorUE4Plugin : ModuleRules
 		FileReference f = r.GetModuleFileName( this.GetType().Name );
 		//File.WriteAllText("c:/temp/qqq2.txt", f.CanonicalName );
 		
-        string ModulePath = Path.GetDirectoryName( f.CanonicalName );
         string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
-        string ThirdPartyPath = Path.GetFullPath( Path.Combine( ModulePath, "../../ThirdParty/" ) );
+        string ThirdPartyPath = Path.GetFullPath( Path.Combine(this.ModuleDirectory, "../../ThirdParty/" ) );
        
 	    string LibrariesPath = Path.Combine(ThirdPartyPath, "MySQLConnector", "Lib");
 
-        string LibraryName = Path.Combine(LibrariesPath, "mariadbclient." + PlatformString + ".lib");
-        PublicAdditionalLibraries.Add(LibraryName);
+        if (Target.Platform == UnrealTargetPlatform.Win64 || Target.Platform == UnrealTargetPlatform.Win32) {
+            string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
+            string LibraryName = Path.Combine(LibrariesPath, "mariadbclient." + PlatformString + ".lib");
+            PublicAdditionalLibraries.Add(LibraryName);
+        } else if (Target.Platform == UnrealTargetPlatform.Linux) {
+            string LinuxPath = Path.Combine(ThirdPartyPath, "LinuxConnector", "lib");
+            string LibraryName = Path.Combine(LinuxPath, "libmysqlclient.so");
+            PublicAdditionalLibraries.Add(LibraryName);
+        }
 
         PrivateIncludePaths.AddRange(new string[] { "MySQLConnectorUE4Plugin/Private" });
         PublicIncludePaths.AddRange(new string[] { "MySQLConnectorUE4Plugin/Public" });
